4f7a1c12c70b07a4dbe7a56d49f6f4d08312958e
root <root@DESKTOP-JP721TN.localdomain>
2020-02-29 13:35:46 +0800

    豆瓣地址解析。

--- src/views/MovieEditor.vue
+++ src/views/MovieEditor.vue
@@ -1,72 +1,8 @@
 <template>
-  <el-row>
+  <el-row v-loading="loading">
     <el-col :span="12" :offset="4">
       <el-form v-bind:model="movie" label-position="top">
-        <el-row :gutter="20">
-          <el-col :span="12">
-            <el-form-item label="中文名称">
-              <el-input v-model="movie.chineseName"></el-input>
-            </el-form-item>
-          </el-col>
-          <el-col :span="12">
-            <el-form-item label="源名称">
-              <el-input v-model="movie.originName"></el-input>
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row :gutter="20">
-          <el-col :span="8">
-            <el-form-item label="导演">
-              <el-button v-on:click="movie.directors.push({})">增加</el-button>
-              <el-button v-on:click="movie.directors.pop()">减少</el-button>
-            </el-form-item>
-            <el-form-item v-for="(director, index) in movie.directors" v-bind:key="index">
-              <el-input v-model="director.name"></el-input>
-            </el-form-item>
-          </el-col>
-          <el-col :span="8">
-            <el-form-item label="主演">
-              <el-button v-on:click="movie.starrings.push({})">增加</el-button>
-              <el-button v-on:click="movie.starrings.pop()">减少</el-button>
-            </el-form-item>
-            <el-form-item v-for="(starring, index) in movie.starrings" v-bind:key="index">
-              <el-input v-model="starring.name"></el-input>
-            </el-form-item>
-          </el-col>
-          <el-col :span="8">
-            <el-form-item label="国家">
-              <el-button v-on:click="movie.countries.push({})">增加</el-button>
-              <el-button v-on:click="movie.countries.pop()">减少</el-button>
-            </el-form-item>
-            <el-form-item v-for="(country, index) in movie.countries" v-bind:key="index">
-              <el-input v-model="country.name"></el-input>
-            </el-form-item>
-          </el-col>
-        </el-row>
-
-        <el-form-item label="标签">
-          <el-checkbox-group v-model="movie.tags">
-            <el-checkbox v-bind:label="tag" v-for="(tag, index) in tags" v-bind:key="index"></el-checkbox>
-          </el-checkbox-group>
-        </el-form-item>
-        <el-row :gutter="20">
-          <el-col :span="8">
-            <el-form-item label="上映日期">
-              <el-date-picker v-model="movie.releaseDate" type="date"></el-date-picker>
-            </el-form-item>
-          </el-col>
-          <el-col :span="8">
-            <el-form-item label="评分">
-              <el-input-number
-                v-model="movie.score"
-                :step="0.1"
-                :min="0"
-                :max="10"
-                :precision="1"
-                :controls="false"
-              ></el-input-number>
-            </el-form-item>
-          </el-col>
+        <el-row>
           <el-col :span="8">
             <el-form-item label="类型">
               <el-select v-model="movie.type">
@@ -79,37 +15,33 @@
               </el-select>
             </el-form-item>
           </el-col>
+          <el-col :span="16">
+            <el-form-item label="豆瓣地址">
+              <el-input v-model="movie.doubanUrl"></el-input>
+            </el-form-item>
+          </el-col>
         </el-row>
-        <el-form-item label="简介">
-          <el-input type="textarea" v-bind:autosize="{minRows: 4}" v-model="movie.introduction"></el-input>
-        </el-form-item>
-
         <el-form-item label="上线">
-          <el-button v-on:click="movie.launches.push({})">增加</el-button>
+          <el-button v-on:click="movie.launches.push({name:''})">增加</el-button>
           <el-button v-on:click="movie.launches.pop()">减少</el-button>
         </el-form-item>
         <el-row :gutter="20">
           <el-col :span="8" v-for="(launch, index) in movie.launches" v-bind:key="index">
             <el-form-item label="类型">
-              <el-input v-model="launch.type"></el-input>
+              <el-select v-model="launch.type">
+                <el-option
+                  v-for="(launch,index) in launches"
+                  v-bind:key="index"
+                  v-bind:label="launch.label"
+                  v-bind:value="launch.value"
+                ></el-option>
+              </el-select>
             </el-form-item>
             <el-form-item label="日期">
               <el-date-picker v-model="launch.date" type="date"></el-date-picker>
             </el-form-item>
           </el-col>
         </el-row>
-
-        <el-form-item label="海报链接">
-          <el-input v-model="movie.posterUrl"></el-input>
-        </el-form-item>
-        <el-form-item label="海报预览">
-          <el-image v-bind:src="movie.posterUrl">
-            <div slot="error">
-              <i class="el-icon-picture-outline"></i>
-            </div>
-          </el-image>
-        </el-form-item>
-
         <el-form-item>
           <el-button v-on:click="submit()">提交</el-button>
         </el-form-item>
@@ -122,9 +54,10 @@ export default {
   name: "Movie",
   data: function() {
     return {
-      active: 1,
+      loading: false,
       movie: {
-        id: 1,
+        doubanUrl: "",
+        id: 0,
         chineseName: "",
         countries: [],
         directors: [],
@@ -148,44 +81,41 @@ export default {
           label: "电视剧"
         }
       ],
-      tags: [
-        "剧情",
-        "喜剧",
-        "动作",
-        "爱情",
-        "科幻",
-        "动画",
-        "悬疑",
-        "惊悚",
-        "恐怖",
-        "犯罪",
-        "同性",
-        "音乐",
-        "歌舞",
-        "传记",
-        "历史",
-        "战争",
-        "西部",
-        "奇幻",
-        "冒险",
-        "灾难",
-        "武侠",
-        "情色"
+      launches: [
+        {
+          value: "蓝光",
+          label: "蓝光"
+        },
+        {
+          value: "美版",
+          label: "美版"
+        },
+        {
+          value: "韩版",
+          label: "韩版"
+        },
+        {
+          value: "Netflix",
+          label: "Netflix"
+        }
       ]
     };
   },
   methods: {
     submit: function() {
+      this.loading = true;
       this.$axios
-        .post(
-          "/movie/",
-          this.movie,
-          function() {
+        .post("/movie/", this.movie)
+        .then(
+          function(response) {
+            this.loading = false;
             this.$message("提交成功！");
+            this.movie = response.data.object;
           }.bind(this)
         )
         .catch(
           function() {
+            this.loading = false;
             this.$message("提交失败！");
           }.bind(this)
         );
