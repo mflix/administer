b340776a8f10938d1b21bf22fb9923573b499b7d
root <root@DESKTOP-JP721TN.localdomain>
2020-03-02 19:59:17 +0800

    子类型自动获取。

--- src/views/MovieEditor.vue
+++ src/views/MovieEditor.vue
@@ -2,33 +2,17 @@
   <el-row v-loading="loading">
     <el-col :span="12" :offset="4">
       <el-form v-bind:model="movie" label-position="top">
-        <el-row>
-          <el-col :span="8">
-            <el-form-item label="类型">
-              <el-select v-model="movie.type">
-                <el-option
-                  v-for="(type,index) in types"
-                  v-bind:key="index"
-                  v-bind:label="type.label"
-                  v-bind:value="type.value"
-                ></el-option>
-              </el-select>
-            </el-form-item>
-          </el-col>
-          <el-col :span="16">
-            <el-form-item label="豆瓣地址">
-              <el-input v-model="movie.doubanUrl"></el-input>
-            </el-form-item>
-          </el-col>
-        </el-row>
+        <el-form-item label="豆瓣地址">
+          <el-input v-model="doubanUrl"></el-input>
+        </el-form-item>
         <el-form-item label="上线">
-          <el-button v-on:click="movie.launches.push({name:''})">增加</el-button>
+          <el-button v-on:click="movie.launches.push({})">增加</el-button>
           <el-button v-on:click="movie.launches.pop()">减少</el-button>
         </el-form-item>
         <el-row :gutter="20">
           <el-col :span="8" v-for="(launch, index) in movie.launches" v-bind:key="index">
-            <el-form-item label="类型">
-              <el-select v-model="launch.type">
+            <el-form-item label="版本">
+              <el-select v-model="launch.version">
                 <el-option
                   v-for="(launch,index) in launches"
                   v-bind:key="index"
@@ -55,32 +39,11 @@ export default {
   data: function() {
     return {
       loading: false,
+      doubanUrl: "",
       movie: {
-        doubanUrl: "",
-        id: 0,
-        chineseName: "",
-        countries: [],
-        directors: [],
-        introduction: "",
-        launches: [],
-        originName: "",
-        posterUrl: "",
-        releaseDate: "",
-        score: 0.0,
-        starrings: [],
-        tags: [],
-        type: ""
+        subject: "",
+        launches: []
       },
-      types: [
-        {
-          value: "电影",
-          label: "电影"
-        },
-        {
-          value: "剧集",
-          label: "剧集"
-        }
-      ],
       launches: [
         {
           value: "蓝光",
@@ -104,6 +67,7 @@ export default {
   methods: {
     submit: function() {
       this.loading = true;
+      this.movie.subject = /\/([0-9]+)\//.exec(this.doubanUrl)[1];
       this.$axios
         .post("/movie/", this.movie)
         .then(
